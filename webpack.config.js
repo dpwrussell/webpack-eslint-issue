140fff24c38e834549d9912943ffcb4a0e2c2362
dpwrussell <dpwrussell@gmail.com>
2020-04-21 18:48:58 +0100

    Added babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,16 @@ module.exports = {
                 loader: "eslint-loader"
             },
             {
+                test: /\.?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.css$/,
                 use: [
                     'style-loader',
